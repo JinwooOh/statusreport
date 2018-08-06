d9b787d30b3f2974ffd4960454e0783eaf6e2ea2
Jinwoo <ozi0414@gmail.com>
2018-08-06 11:33:29 -0500

    edit readme

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-// dev.js - don't commit this
-module.exports = {
-  host: 'wwwtest.eipd.dcs.wisc.edu',
-  user: 'status_report',
-  database: 'status_report',
-  port: '3306',
-  password: 'status_report1234',
-};
