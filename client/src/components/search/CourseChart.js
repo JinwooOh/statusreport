c12b97a502118bb1a8923eb2db7c9cc9755ecccd
Jinwoo <ozi0414@gmail.com>
2018-08-27 09:51:13 -0500

    add chart for program search

--- client/src/components/search/CourseChart.js
+++ client/src/components/search/CourseChart.js
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Polar } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
 
 // chart for user search summary
 class CourseChart extends Component {
   render() {
-    console.log(this.props.programList);
-    // const courseData = {
-    //   labels: [
-    //     // this.props.courseList.list[0].name,
-    //     // this.props.courseList.list[1].name,
-    //     // this.props.courseList.list[2].name,
-    //     // this.props.courseList.list[3].name,
-    //     // this.props.courseList.list[4].name,
-    //   ],
-    //   datasets: [
-    //     {
-    //       data: [
-    //         // this.props.courseList.list[0].total,
-    //         // this.props.courseList.list[1].total,
-    //         // this.props.courseList.list[2].total,
-    //         // this.props.courseList.list[3].total,
-    //         // this.props.courseList.list[4].total,
-    //       ],
-    //       backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#ff9063', '#63d2ff'],
-    //       hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#ff9063', '#63d2ff'],
-    //     },
-    //   ],
-    // };
-
-    return <div>{/* <Polar data="" /> */}</div>;
+    const courseData = {
+      labels: [],
+      datasets: [
+        {
+          data: [],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#ff9063', '#63d2ff'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#ff9063', '#63d2ff'],
+        },
+      ],
+    };
+    const { programList } = this.props;
+    // iterate object
+    for (const program in programList) {
+      if (programList.hasOwnProperty(program)) {
+        courseData.labels.push(program); // name of the program
+        courseData.datasets[0].data.push(programList[program].total); // total hours for the program
+      }
+    }
+    console.log(courseData);
+    return (
+      <div>
+        <Pie data={courseData} />
+      </div>
+    );
   }
 }
 
