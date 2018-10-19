297d0b0550fabc5ab82ef1a21da4e385e3dee404
Jinwoo <ozi0414@gmail.com>
2018-10-19 14:53:09 -0500

    add active tab color styling

--- client/src/components/report/TaskSelector.js
+++ client/src/components/report/TaskSelector.js
@@ -4,13 +4,30 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class TaskSelector extends Component {
+  handleTaskSelector = (e, taskType) => {
+    const taskSelector = document.getElementsByClassName('btn__taskSelector');
+    console.log(taskSelector);
+    for (const task of taskSelector) {
+      task.className = task.className.replace(' selected', '');
+    }
+    console.log(e);
+    e.currentTarget.className += ' selected';
+    this.props.selectTask(taskType);
+  };
+
   render() {
     return (
       <div className="taskSelector">
-        <button className="btn btn__taskSelector" onClick={() => this.props.selectTask('course')}>
+        <button
+          className="btn btn__taskSelector"
+          onClick={e => this.handleTaskSelector(e, 'course')}
+        >
           Course
         </button>
-        <button className="btn btn__taskSelector" onClick={() => this.props.selectTask('admin')}>
+        <button
+          className="btn btn__taskSelector"
+          onClick={e => this.handleTaskSelector(e, 'admin')}
+        >
           Administration
         </button>
       </div>
--- client/src/components/search/SearchSelector.js
+++ client/src/components/search/SearchSelector.js
@@ -5,15 +5,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SearchSelector extends Component {
+  handleSearchSelector = (e, searchType) => {
+    const taskSelector = document.getElementsByClassName('btn__taskSelector');
+    console.log(taskSelector);
+    for (const task of taskSelector) {
+      task.className = task.className.replace(' selected', '');
+    }
+    console.log(e);
+    e.currentTarget.className += ' selected';
+    this.props.selectSearch(searchType);
+  };
+
   render() {
     return (
       <div className="taskSelector">
-        <button className="btn btn__taskSelector" onClick={() => this.props.selectSearch('user')}>
+        <button
+          className="btn btn__taskSelector"
+          onClick={e => this.handleSearchSelector(e, 'user')}
+        >
           User
         </button>
         <button
           className="btn btn__taskSelector"
-          onClick={() => this.props.selectSearch('program')}
+          onClick={e => this.handleSearchSelector(e, 'program')}
         >
           Course
         </button>
