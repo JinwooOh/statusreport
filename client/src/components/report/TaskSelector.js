dcd5ac687b9f71530187d7d3962420dabb0764f9
Jinwoo <ozi0414@gmail.com>
2018-11-02 13:54:29 -0500

    updated audit fix

--- client/src/components/report/TaskSelector.js
+++ client/src/components/report/TaskSelector.js
@@ -6,11 +6,9 @@ import PropTypes from 'prop-types';
 class TaskSelector extends Component {
   handleTaskSelector = (e, taskType) => {
     const taskSelector = document.getElementsByClassName('btn__taskSelector');
-    console.log(taskSelector);
     for (const task of taskSelector) {
       task.className = task.className.replace(' selected', '');
     }
-    console.log(e);
     e.currentTarget.className += ' selected';
     this.props.selectTask(taskType);
   };
